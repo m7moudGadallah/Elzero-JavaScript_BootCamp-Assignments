a83a1d509a30aba861f9d344f831222fc3a74164
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-03-21 04:05:30 +0200

    Add `loops` assignment3
    - Add assignment3 directory
        |_ README file
        |_ main.js
    - Modify 'loops/README.md' `adding link to Assignment3`
    - Add image to 'loops/pic' related to this assignment

---
+++ loops/assignment3/main.js
@@ -0,0 +1,10 @@
+let start = 1;
+let end = 6;
+let breaker = 2;
+
+// Output
+for (let i = start; i <= end; i++) {
+    console.log(i);
+    console.log(`-- ${breaker}`);
+    console.log(`-- ${breaker+breaker}`);
+}
